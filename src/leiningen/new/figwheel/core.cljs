05d2d869908bc1d650bbd53980667a37bffe820b
Bruce Hauman <bhauman@gmail.com>
2014-05-10 15:38:29 -0400

    reps update

--- src/leiningen/new/figwheel/core.cljs
+++ src/leiningen/new/figwheel/core.cljs
@@ -1,11 +1,11 @@
 (ns {{name}}.core
   (:require
-    [figwheel.client :as fw :include-macros true]))
+    [figwheel.client :as fw]))
 
 (enable-console-print!)
 
-;; define your app data so that it doesn't get over written on reload
-;; (fw/defonce app-data (atom {}))
+;; define your app data so that it doesn't get over-written on reload
+;; (defonce app-data (atom {}))
 
 (println "Edits to this text should show up in your developer console.")
 
--- src/leiningen/new/figwheel/project.clj
+++ src/leiningen/new/figwheel/project.clj
@@ -3,11 +3,11 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/clojurescript "0.0-2202"]
                  [figwheel "0.1.2-SNAPSHOT"]
                  [org.clojure/core.async "0.1.278.0-76b25b-alpha"]
-                 [sablono "0.1.5"]
+                 [sablono "0.2.16"]
                  [om "0.6.2"]]
   
   :plugins [[lein-cljsbuild "1.0.3"]
