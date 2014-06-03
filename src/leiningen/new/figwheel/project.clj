7ca0379ca5739c40f9181bcb7ff8176cd0540639
Bruce Hauman <bhauman@gmail.com>
2014-06-03 15:27:52 -0400

    ooops needed to bump dependency

--- src/leiningen/new/figwheel/project.clj
+++ src/leiningen/new/figwheel/project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/clojurescript "0.0-2202"]
-                 [figwheel "0.1.2-SNAPSHOT"]
+                 [figwheel "0.1.3-SNAPSHOT"]
                  [org.clojure/core.async "0.1.278.0-76b25b-alpha"]
                  [sablono "0.2.16"]
                  [om "0.6.2"]]
