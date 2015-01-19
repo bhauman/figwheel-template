fbbe1af41a41de5cff273d98681a9407db09b470
Bruce Hauman <bhauman@gmail.com>
2015-01-19 18:38:02 -0500

    updating project

--- src/leiningen/new/figwheel/project.clj
+++ src/leiningen/new/figwheel/project.clj
@@ -23,17 +23,37 @@
               :compiler {:output-to "resources/public/js/compiled/{{sanitized}}.js"
                          :output-dir "resources/public/js/compiled/out"
                          :optimizations :none
-                         :source-map true}}
+                         :source-map true
+                         :source-map-timestamp true
+                         :cache-analysis true }}
              {:id "min"
               :source-paths ["src"]
               :compiler {:output-to "www/{{sanitized}}.min.js"
                          :optimizations :advanced
-                         :pretty-print false
-                         :preamble ["react/react.min.js"]
-                         :externs ["react/externs/react.js"]}}]}
+                         :pretty-print false}}]}
+
   :figwheel {
              :http-server-root "public" ;; default and assumes "resources" 
              :server-port 3449 ;; default
              :css-dirs ["resources/public/css"] ;; watch and update CSS
-             ;; :ring-handler {{name}}.server/handler
+
+             ;; Server Ring Handler (optional)
+             ;; if you want to embed a ring handler into the figwheel http-kit
+             ;; server
+             ;; :ring-handler hello_world.server/handler
+
+             ;; To be able to open files in your editor from the heads up display
+             ;; you will need to put a script on your path.
+             ;; that script will have to take a file path and a line number
+             ;; ie. in  ~/bin/myfile-opener
+             ;; #! /bin/sh
+             ;; emacsclient -n +$2 $1
+             ;;
+             ;; :open-file-command "myfile-opener"
+
+             ;; if you want to disable the REPL
+             ;; :repl false
+
+             ;; to configure a different figwheel logfile path
+             ;; :server-logfile "tmp/logs/figwheel-logfile.log" 
              })
