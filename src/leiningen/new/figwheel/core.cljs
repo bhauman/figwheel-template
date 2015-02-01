719a8095e94958141b84f693abddc3b21294dcf5
Sebastian Bensusan <sbensu@gmail.com>
2015-02-01 13:33:27 +0100

    Added print message to core.cljs

--- src/leiningen/new/figwheel/core.cljs
+++ src/leiningen/new/figwheel/core.cljs
@@ -7,6 +7,10 @@
 
 (enable-console-print!)
 
+(println "Edits to this text should show up in your developer console.")
+
+;; define your app data so that it doesn't get over-written on reload
+
 (defonce app-state (atom {:text "Hello world!"}))
 {{#om?}}
 
