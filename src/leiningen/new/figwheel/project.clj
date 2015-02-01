471ef0b482ba44486631889c74e01c3a83e48bbf
Sebastian Bensusan <sbensu@gmail.com>
2015-02-01 14:14:50 +0100

    Added React for reagent via a script tag in index.hmtl. This will not be necessary in the next stable version, 0.5.0

--- src/leiningen/new/figwheel/project.clj
+++ src/leiningen/new/figwheel/project.clj
@@ -8,9 +8,9 @@
                  [org.clojure/clojurescript "0.0-2727"]
                  [figwheel "0.2.2-SNAPSHOT"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]{{#om?}}
+                 [sablono "0.2.22"]
                  [org.omcljs/om "0.8.6"]{{/om?}}{{#reagent?}}
-                 [reagent "0.5.0-alpha"]{{/reagent?}}
-                 [sablono "0.2.22"]]
+                 [reagent "0.4.3"]{{/reagent?}}]
 
   :plugins [[lein-cljsbuild "1.0.4"]
             [lein-figwheel "0.2.2-SNAPSHOT"]]
