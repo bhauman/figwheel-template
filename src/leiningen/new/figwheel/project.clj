7bfb61b0c7c2313b22e5bfc779b59d81fa53601e
Sebastian Bensusan <sbensu@gmail.com>
2015-02-02 11:07:02 +0100

    Added assets-path option to compiler and removed extra tags in index.html

--- src/leiningen/new/figwheel/project.clj
+++ src/leiningen/new/figwheel/project.clj
@@ -25,6 +25,8 @@
               :compiler {:output-to "resources/public/js/compiled/{{sanitized}}.js"
                          :output-dir "resources/public/js/compiled/out"
                          :optimizations :none
+                         :main {{name}}.core
+                         :asset-path "js/compiled/out"
                          :source-map true
                          :source-map-timestamp true
                          :cache-analysis true }}
