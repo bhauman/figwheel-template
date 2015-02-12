a78e5bfb3bff811e158719678ec06ed42bd11fba
pkillean <pkillean@gmail.com>
2015-02-12 14:57:29 -0500

    add socket config so index.html works ootb

--- src/leiningen/new/figwheel/dev_core.cljs
+++ src/leiningen/new/figwheel/dev_core.cljs
@@ -4,6 +4,7 @@
      [figwheel.client :as fw]))
 
 (fw/start {
+  :websocket-url "ws://localhost:3449/figwheel-ws"
   :on-jsload (fn []
                ;; (stop-and-start-my app)
                )})
