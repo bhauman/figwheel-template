cea30f57cef6162905bdbd69b5cbf9a6a6dd7a22
Bruce Hauman <bhauman@gmail.com>
2015-05-01 11:10:29 -0400

    updating cljs dep

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject figwheel/lein-template "0.3.0-1"
+(defproject figwheel/lein-template "0.3.0-2"
   :description "A Leinigen template for figwheel"
   :url "https://github.com/bhauman/figwheel-template"
   :license {:name "Eclipse Public License"
--- src/leiningen/new/figwheel/project.clj
+++ src/leiningen/new/figwheel/project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-3195"]
+                 [org.clojure/clojurescript "0.0-3211"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]{{#om?}}
                  [sablono "0.3.4"]
                  [org.omcljs/om "0.8.8"]{{/om?}}{{#reagent?}}
