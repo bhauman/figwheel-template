29177d16dd1d7605d5f44f915716a5fc4f99bf6d
Bruce Hauman <bhauman@gmail.com>
2015-10-08 12:06:04 -0400

    bumping figwheel version

--- src/leiningen/new/figwheel/project.clj
+++ src/leiningen/new/figwheel/project.clj
@@ -12,7 +12,7 @@
                  [reagent "0.5.0"]{{/reagent?}}]
 
   :plugins [[lein-cljsbuild "1.1.0"]
-            [lein-figwheel "0.4.0"]]
+            [lein-figwheel "0.4.1"]]
 
   :source-paths ["src"]
 
