45d1de6e060390857b2bbb2a35c23a0e6bd20a32
Bruce Hauman <bhauman@gmail.com>
2018-05-06 20:17:01 -0400

    bump deps

--- src/leiningen/new/figwheel/project.clj
+++ src/leiningen/new/figwheel/project.clj
@@ -12,7 +12,7 @@
   :min-lein-version "2.7.1"
 
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojure/clojurescript "1.9.946"]
+                 [org.clojure/clojurescript "1.10.238"]
                  [org.clojure/core.async  "0.4.474"]{{#react?}}
                  [cljsjs/react "15.6.1-1"]
                  [cljsjs/react-dom "15.6.1-1"]
@@ -24,7 +24,7 @@
                  [reagent "0.7.0"]{{/reagent?}}{{#rum?}}
                  [rum "0.11.2"]{{/rum?}}]
 
-  :plugins [[lein-figwheel "0.5.15"]
+  :plugins [[lein-figwheel "0.5.16"]
             [lein-cljsbuild "1.1.7" :exclusions [[org.clojure/clojure]]]]
 
   :source-paths ["src"]
@@ -105,8 +105,8 @@
   ;; Please see:
   ;; https://github.com/bhauman/lein-figwheel/wiki/Using-the-Figwheel-REPL-within-NRepl
   :profiles {:dev {:dependencies [[binaryage/devtools "0.9.9"]
-                                  [figwheel-sidecar "0.5.15"]
-                                  [com.cemerick/piggieback "0.2.2"]]
+                                  [figwheel-sidecar "0.5.16"]
+                                  [cider/piggieback "0.3.1"]]
                    ;; need to add dev source path here to get user.clj loaded
                    :source-paths ["src" "dev"]
                    ;; for CIDER
