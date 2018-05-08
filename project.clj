da0288594ca69e8b8da586d9e3ffa35b35e3e148
Bruce Hauman <bhauman@gmail.com>
2018-05-08 07:55:27 -0400

    cider piggieback

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject figwheel/lein-template "0.5.16"
+(defproject figwheel/lein-template "0.5.16-1"
   :description "A Leinigen template for figwheel"
   :url "https://github.com/bhauman/figwheel-template"
   :license {:name "Eclipse Public License"
--- src/leiningen/new/figwheel/project.clj
+++ src/leiningen/new/figwheel/project.clj
@@ -111,7 +111,7 @@
                    :source-paths ["src" "dev"]
                    ;; for CIDER
                    ;; :plugins [[cider/cider-nrepl "0.12.0"]]
-                   :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
+                   :repl-options {:nrepl-middleware [cider.piggieback/wrap-cljs-repl]}
                    ;; need to add the compliled assets to the :clean-targets
                    :clean-targets ^{:protect false} ["resources/public/js/compiled"
                                                      :target-path]}})
