2321d8107cc62e73f4efa52cf7eba700e53367e6
Bruce Hauman <bhauman@gmail.com>
2020-06-17 08:39:00 -0600

    bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject figwheel/lein-template "0.5.20-2"
+(defproject figwheel/lein-template "0.5.20-3"
   :description "A Leinigen template for figwheel"
   :url "https://github.com/bhauman/figwheel-template"
   :license {:name "Eclipse Public License"
